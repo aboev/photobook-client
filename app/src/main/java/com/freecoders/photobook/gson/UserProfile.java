8652857ac7b3debc888ba00011238c9f8909c3b2
Aleksey Boev <aboyev@laptop>
2014-11-27 22:53:42 +0900

    - UserProfile initial commit (Refer to RESTful API docs for description)

---
+++ app/src/main/java/com/freecoders/photobook/gson/UserProfile.java
@@ -0,0 +1,12 @@
+package com.freecoders.photobook.gson;
+
+/**
+ * Created by Alex on 2014-11-27.
+ */
+public class UserProfile {
+    public String name = "";
+    public String phone = "";
+    public String email = "";
+    public String avatar = "";
+    public String id = "";
+}
