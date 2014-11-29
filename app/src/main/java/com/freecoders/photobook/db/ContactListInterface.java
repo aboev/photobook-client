2de898484e0372114f71467386847cb2a89bf424
Aleksey Boev <aboyev@laptop>
2014-11-29 10:37:44 +0900

    - ContactListInterface initial commit

---
+++ app/src/main/java/com/freecoders/photobook/db/ContactListInterface.java
@@ -0,0 +1,19 @@
+package com.freecoders.photobook.db;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alex on 2014-11-27.
+ */
+public class ContactListInterface {
+    public static final ArrayList<String> getContactList(Context context){
+        //Returns list of phone numers from Android contact list
+        return null;
+    }
+}
