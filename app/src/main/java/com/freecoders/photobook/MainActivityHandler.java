5e033f8cc9bed0e36371dca2120ad26ad5b6914c
Aleksey Boev <aboyev@laptop>
2014-11-30 18:53:54 +0900

    - Removed refreshContactList()

--- app/src/main/java/com/freecoders/photobook/MainActivityHandler.java
+++ app/src/main/java/com/freecoders/photobook/MainActivityHandler.java
@@ -3,9 +3,9 @@ package com.freecoders.photobook;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.freecoders.photobook.common.Constants;
+import com.freecoders.photobook.common.Photobook;
 import com.freecoders.photobook.common.Preferences;
 import com.freecoders.photobook.db.ContactListInterface;
-import com.freecoders.photobook.db.ContactsRetriever;
 import com.freecoders.photobook.db.FriendEntry;
 import com.freecoders.photobook.gson.UserProfile;
 import com.freecoders.photobook.network.ServerInterface;
@@ -31,71 +31,12 @@ public class MainActivityHandler {
 	
 	public void init(MainActivity activity) {
         this.activity = activity;
-		prefs = new Preferences(activity);
+        Photobook.setMainActivity(activity);
+        prefs = new Preferences(activity);
 		if (!prefs.loadPreferences()) {
 			Intent intent = new Intent(activity, RegisterActivity.class);
 		    activity.startActivity(intent);
-		} else {
-            refreshContactList();
-
-        }
+		}
 	}
-
-    public void refreshContactList(){
-        if (prefs.strUserID.isEmpty()) return;
-
-        //final ArrayList<String> contactList = ContactListInterface.getContactList(activity);
-        ContactsRetriever contactsRet = new ContactsRetriever();
-        final ArrayList<String> contactList = contactsRet.getContacts(activity);
-        Log.d(Constants.LOG_TAG,"Contacts retrieved");
-        //final ArrayList<String> contactList = new ArrayList<String>();
-        //for (int i = 111; i <= 129; i++) contactList.add(String.valueOf(i));
-
-        for (int i = 0; i < contactList.size(); i++) {
-            if (activity.friendsDataSource.getFriendByContactKey(contactList.get(i))==null) {
-                activity.friendsDataSource.createFriend("", contactList.get(i), "", "",
-                        FriendEntry.INT_STATUS_NULL);
-            }
-        }
-
-        ServerInterface.postContactsRequest(activity,
-                contactList, prefs.strUserID,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.d(Constants.LOG_TAG, "Response from server " + response.toString());
-                        try {
-                            JSONObject obj = new JSONObject(response);
-                            JSONObject objMap = obj.getJSONObject("data");
-                            Map<String, UserProfile> retMap =
-                               new Gson().fromJson(objMap.toString(),
-                                    new TypeToken<HashMap<String, UserProfile>>() {}.getType());
-                            for (int i = 0; i < contactList.size(); i++){
-                                if (retMap.containsKey(contactList.get(i))) {
-                                   FriendEntry friend = activity.friendsDataSource.
-                                           getFriendByContactKey(contactList.get(i));
-                                    UserProfile profile = retMap.get(contactList.get(i));
-                                    if (friend != null){
-
-                                        friend.setName(profile.name);
-                                        friend.setAvatar(profile.avatar);
-                                        friend.setUserId(profile.id);
-                                        friend.setStatus(FriendEntry.INT_STATUS_DEFAULT);
-                                        activity.friendsDataSource.updateFriend(friend);
-                                    }
-                                }
-                            }
-                        } catch (Exception e) {
-                            Log.d(Constants.LOG_TAG, "Exception" + e.getLocalizedMessage());
-                        }
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d(Constants.LOG_TAG, "Error: " + error.getMessage());
-                    }
-                });
-
-    }
 
 }
