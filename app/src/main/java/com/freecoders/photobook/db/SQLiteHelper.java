0d8ae42bf1da604fc1701a8648a4a4609a870f47
Aleksey Boev <aboyev@laptop>
2014-12-06 22:53:47 +0900

    - Added TABLE_IMAGES, updated DB version and supporting methods

--- app/src/main/java/com/freecoders/photobook/db/SQLiteHelper.java
+++ app/src/main/java/com/freecoders/photobook/db/SQLiteHelper.java
@@ -11,18 +11,27 @@ import android.util.Log;
 public class SQLiteHelper extends SQLiteOpenHelper {
 
     public static final String TABLE_FRIENDS = "friends";
+    public static final String TABLE_IMAGES = "images";
+
     public static final String COLUMN_ID = "_id";
+
     public static final String COLUMN_NAME = "Name";
     public static final String COLUMN_CONTACT_KEY = "ContactKey";
     public static final String COLUMN_USER_ID = "UserId";
     public static final String COLUMN_AVATAR = "Avatar";
     public static final String COLUMN_STATUS = "Status";
 
+    public static final String COLUMN_MEDIASTORE_ID = "mediaStoreId";
+    public static final String COLUMN_ORIG_URI = "origUri";
+    public static final String COLUMN_THUMB_URI = "thumbUri";
+    public static final String COLUMN_SERVER_ID = "serverId";
+    public static final String COLUMN_TITLE = "title";
+
     private static final String DATABASE_NAME = "photobook.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 4;
 
     // Database creation sql statement
-    private static final String DATABASE_CREATE = "create table "
+    private static final String DATABASE_CREATE_FRIENDS_TABLE = "create table "
             + TABLE_FRIENDS + "("
             + COLUMN_ID + " integer primary key autoincrement, "
             + COLUMN_NAME + " varchar(1000), "
@@ -31,13 +40,24 @@ public class SQLiteHelper extends SQLiteOpenHelper {
             + COLUMN_AVATAR + " varchar(3000), "
             + COLUMN_STATUS + " int);";
 
+    private static final String DATABASE_CREATE_IMAGES_TABLE = "create table "
+            + TABLE_IMAGES + "("
+            + COLUMN_ID + " integer primary key autoincrement, "
+            + COLUMN_MEDIASTORE_ID + " integer, "
+            + COLUMN_ORIG_URI + " text, "
+            + COLUMN_THUMB_URI + " text, "
+            + COLUMN_SERVER_ID + " text, "
+            + COLUMN_TITLE + " text, "
+            + COLUMN_STATUS + " int);";
+
     public SQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase database) {
-        database.execSQL(DATABASE_CREATE);
+        database.execSQL(DATABASE_CREATE_FRIENDS_TABLE);
+        database.execSQL(DATABASE_CREATE_IMAGES_TABLE);
     }
 
     @Override
@@ -46,6 +66,7 @@ public class SQLiteHelper extends SQLiteOpenHelper {
                 "Upgrading database from version " + oldVersion + " to "
                         + newVersion + ", which will destroy all old data");
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_FRIENDS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_IMAGES);
         onCreate(db);
     }
 
