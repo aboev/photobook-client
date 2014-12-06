d8a0ae6a4ef1b223e1559fd8479213ea82e565d0
Aleksey Boev <aboyev@laptop>
2014-12-06 22:49:10 +0900

    - Added methods getPreferences(), getImageDataSource()

--- app/src/main/java/com/freecoders/photobook/common/Photobook.java
+++ app/src/main/java/com/freecoders/photobook/common/Photobook.java
@@ -5,24 +5,35 @@ import android.app.Application;
 import com.freecoders.photobook.FriendsFragmentTab;
 import com.freecoders.photobook.MainActivity;
 import com.freecoders.photobook.db.FriendsDataSource;
+import com.freecoders.photobook.db.ImagesDataSource;
 
 /**
  * Created by Alex on 2014-11-30.
  */
 public class Photobook extends Application {
     private static FriendsDataSource friendsDataSource;
+    private static ImagesDataSource imagesDataSource;
     private static FriendsFragmentTab mFriendsTab;
     private static MainActivity mActivity;
+    private static Preferences mPreferences;
 
     @Override
     public void onCreate() {
         super.onCreate();
         friendsDataSource = new FriendsDataSource(this);
         friendsDataSource.open();
+        imagesDataSource = new ImagesDataSource(this);
+        imagesDataSource.open();
+        mPreferences = new Preferences(this);
+        mPreferences.loadPreferences();
     }
 
     public final static FriendsDataSource getFriendsDataSource(){
         return friendsDataSource;
+    }
+
+    public final static ImagesDataSource getImagesDataSource(){
+        return imagesDataSource;
     }
 
     public final static void setMainActivity(MainActivity activity){
@@ -40,4 +51,6 @@ public class Photobook extends Application {
     public final static FriendsFragmentTab getFriendsFragmentTab(){
         return mFriendsTab;
     }
+
+    public final static Preferences getPreferences() {return mPreferences;}
 }
