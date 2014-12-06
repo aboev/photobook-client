776dc01097b6e7412c647d1d8782b158679264c1
Aleksey Boev <aboyev@laptop>
2014-12-06 23:38:14 +0900

    - PhoneUtils initial commit

---
+++ app/src/main/java/com/freecoders/photobook/utils/PhoneUtils.java
@@ -0,0 +1,23 @@
+package com.freecoders.photobook.utils;
+
+/**
+ * Created by Alex on 2014-12-06.
+ */
+public class PhoneUtils {
+
+    // Implement phone number normalization. Ex. 010-1111-2222 -> +82 10-1111-2222
+    public final static String getNormalizedPhoneNumber(){
+        /*
+        Code here
+         */
+        return "";
+    }
+
+    // Implement local ISO country code from SIM
+    private String getCountryISOCode(){
+        /*
+        Code here
+         */
+        return "KR";
+    }
+}
