cea7347733eb85ae346e09fb417e7b2da3804a69
Aleksey Boev <aboyev@laptop>
2014-12-06 22:55:14 +0900

    - Updated parseNetworkResponse() - Added getHeaders()

--- app/src/main/java/com/freecoders/photobook/network/MultiPartRequest.java
+++ app/src/main/java/com/freecoders/photobook/network/MultiPartRequest.java
@@ -1,20 +1,31 @@
 package com.freecoders.photobook.network;
 
+import android.util.Log;
+
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.freecoders.photobook.common.Constants;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Created by maximilian on 11/22/14.
@@ -29,21 +40,27 @@ public class MultiPartRequest extends Request<String>{
     private String mRouteId;
     private Response.Listener mListener;
 
+    private HashMap<String, String> mRequestHeaders = null;
+
     public MultiPartRequest(String url, String filePath,
+                            HashMap<String, String> requestHeaders,
                             Response.Listener<String> listener,
                             Response.ErrorListener errorListener) {
         super(Method.POST, url, errorListener);
 
         mListener = listener;
+        mRequestHeaders = requestHeaders;
         mHttpEntity = buildMultiPartEntity(filePath);
     }
 
     public MultiPartRequest(String url, File file,
+                            HashMap<String, String> requestHeaders,
                             Response.Listener<String> listener,
                             Response.ErrorListener errorListener) {
         super(Method.POST, url, errorListener);
 
         mListener = listener;
+        mRequestHeaders = requestHeaders;
         mHttpEntity = buildMultiPartEntity(file);
     }
 
@@ -67,6 +84,11 @@ public class MultiPartRequest extends Request<String>{
     }
 
     @Override
+    public Map<String, String> getHeaders() {
+        return ((mRequestHeaders == null) ? Collections.<String, String>emptyMap() : mRequestHeaders);
+    }
+
+    @Override
     public byte[] getBody() throws AuthFailureError {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         try {
@@ -79,7 +101,13 @@ public class MultiPartRequest extends Request<String>{
 
     @Override
     protected Response<String> parseNetworkResponse(NetworkResponse response) {
-        return Response.success("Uploaded", getCacheEntry());
+        String parsed;
+        try {
+            parsed = new String(response.data, HttpHeaderParser.parseCharset(response.headers));
+        } catch (UnsupportedEncodingException e) {
+            parsed = new String(response.data);
+        }
+        return Response.success(parsed, HttpHeaderParser.parseCacheHeaders(response));
     }
 
     @Override
