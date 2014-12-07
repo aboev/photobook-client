b07ad089bae27343efcc6885d99ff0ad5762b81d
maximilian <maxsqr@gmail.com>
2014-12-07 17:04:22 +0900

    added addional ServerUrl

--- app/src/main/java/com/freecoders/photobook/common/Constants.java
+++ app/src/main/java/com/freecoders/photobook/common/Constants.java
@@ -5,8 +5,9 @@ public class Constants {
 	public static final String PREFS_NAME = "PhotoBookPrefs";
 	
 	public static final int INTENT_PICK_IMAGE = 1;
-
-    public final static String SERVER_URL = "http://photobook-freecoders.rhcloud.com";
+	
+	//public final static String SERVER_URL = "http://photobook-freecoders.rhcloud.com";
+    public final static String SERVER_URL = "http://dev.snufan.com";
     public final static String SERVER_PATH_USER = "/user";
     public final static String SERVER_PATH_IMAGE = "/image";
     public final static String SERVER_PATH_CONTACTS = "/contacts";
