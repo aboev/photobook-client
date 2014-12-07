72a4f9208228f2a1533c375882e8b8f2452ce8c3
Aleksey Boev <aboyev@laptop>
2014-12-07 14:12:32 +0900

    - Added SERVER_PATH_FRIENDS

--- app/src/main/java/com/freecoders/photobook/common/Constants.java
+++ app/src/main/java/com/freecoders/photobook/common/Constants.java
@@ -5,11 +5,12 @@ public class Constants {
 	public static final String PREFS_NAME = "PhotoBookPrefs";
 	
 	public static final int INTENT_PICK_IMAGE = 1;
-	
-	public final static String SERVER_URL = "http://photobook-freecoders.rhcloud.com";
+
+    public final static String SERVER_URL = "http://photobook-freecoders.rhcloud.com";
     public final static String SERVER_PATH_USER = "/user";
     public final static String SERVER_PATH_IMAGE = "/image";
     public final static String SERVER_PATH_CONTACTS = "/contacts";
+    public final static String SERVER_PATH_FRIENDS = "/friends";
 
     public final static String FILENAME_AVATAR = "avatar.jpg";
 }
