e75e2cc4a9850df029bea179e1664139e3b9efcd
Aleksey Boev <aboyev@laptop>
2014-12-10 22:13:36 +0900

    - Added constant SERVER_PATH_FEED

--- app/src/main/java/com/freecoders/photobook/common/Constants.java
+++ app/src/main/java/com/freecoders/photobook/common/Constants.java
@@ -5,13 +5,13 @@ public class Constants {
 	public static final String PREFS_NAME = "PhotoBookPrefs";
 	
 	public static final int INTENT_PICK_IMAGE = 1;
-	
-	//public final static String SERVER_URL = "http://photobook-freecoders.rhcloud.com";
-    public final static String SERVER_URL = "http://dev.snufan.com";
+
+    public final static String SERVER_URL = "http://photobook-freecoders.rhcloud.com";
     public final static String SERVER_PATH_USER = "/user";
     public final static String SERVER_PATH_IMAGE = "/image";
     public final static String SERVER_PATH_CONTACTS = "/contacts";
     public final static String SERVER_PATH_FRIENDS = "/friends";
+    public final static String SERVER_PATH_FEED = "/feed";
 
     public final static String FILENAME_AVATAR = "avatar.jpg";
 }
