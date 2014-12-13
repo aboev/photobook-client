2493feed9a641d6b63a1d3476c85a81eb045bfb2
Aleksey Boev <aboyev@laptop>
2014-12-13 22:44:00 +0900

    - ImageJson initial commit

---
+++ app/src/main/java/com/freecoders/photobook/gson/ImageJson.java
@@ -0,0 +1,14 @@
+package com.freecoders.photobook.gson;
+
+/**
+ * Created by Alex on 2014-12-10.
+ */
+public class ImageJson {
+    public String id = "";
+    public long timestamp = 0;
+    public String title = "";
+    public String url_original = "";
+    public String url_medium = "";
+    public String url_small = "";
+    public double ratio = 1.0;
+}
