8f610a321bec6b8a2ed414c5f1c4164330cf9a92
Aleksey Boev <aboyev@laptop>
2014-12-24 20:16:59 +0900

    - Added OnItemClickListener for feed list

--- app/src/main/java/com/freecoders/photobook/FeedFragmentTab.java
+++ app/src/main/java/com/freecoders/photobook/FeedFragmentTab.java
@@ -1,20 +1,36 @@
 package com.freecoders.photobook;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.etsy.android.grid.StaggeredGridView;
+import com.freecoders.photobook.common.Constants;
 import com.freecoders.photobook.common.Photobook;
+import com.freecoders.photobook.db.ImageEntry;
 import com.freecoders.photobook.gson.FeedEntryJson;
 import com.freecoders.photobook.network.FeedLoader;
 import com.freecoders.photobook.network.ImageUploader;
+import com.freecoders.photobook.utils.FileUtils;
+import com.freecoders.photobook.utils.ImageUtils;
+import com.google.gson.Gson;
 
+import java.io.File;
 import java.util.ArrayList;
 
 @SuppressLint("NewApi") 
@@ -32,9 +48,23 @@ public class FeedFragmentTab extends Fragment {
         mAdapter = new FeedAdapter(getActivity(), R.layout.item_feed, feedEntryList);
         mFeedLoader = new FeedLoader(listView);
         listView.setFeedComponents(mFeedLoader, mAdapter, feedEntryList);
+        //listView.setOnItemClickListener(OnItemClickListener);
         //mFeedLoader.loadFeed();
         setRetainInstance(true);
         return rootView;
     }
+
+    AdapterView.OnItemClickListener OnItemClickListener
+            = new AdapterView.OnItemClickListener(){
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position,
+                                long id) {
+            FeedEntryJson feedItem = mAdapter.getItem(position);
+            Intent mIntent = new Intent(Photobook.getMainActivity(), ImageDetailsActivity.class);
+            Photobook.setImageDetails(feedItem);
+            startActivity(mIntent);
+        }
+    };
 
 }
