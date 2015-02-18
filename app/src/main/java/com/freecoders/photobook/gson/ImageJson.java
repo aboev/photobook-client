7ab417c1f6603a695ff365207e94e37b40cf62a0
Aleksey Boev <aboyev@laptop>
2015-02-18 13:07:06 +0900

    - Changed id to image_id

--- app/src/main/java/com/freecoders/photobook/gson/ImageJson.java
+++ app/src/main/java/com/freecoders/photobook/gson/ImageJson.java
@@ -4,7 +4,7 @@ package com.freecoders.photobook.gson;
  * Created by Alex on 2014-12-10.
  */
 public class ImageJson {
-    public String id = "";
+    public String image_id = "";
     public long timestamp = 0;
     public String title = "";
     public String url_original = "";
