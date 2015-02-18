8a57cbb790a63ff07478f2bd60a61b026675b593
Aleksey Boev <aboyev@laptop>
2015-02-18 12:40:46 +0900

    - CommentEntryJson initial commit

---
+++ app/src/main/java/com/freecoders/photobook/gson/CommentEntryJson.java
@@ -0,0 +1,11 @@
+package com.freecoders.photobook.gson;
+
+/**
+ * Created by Alex on 2015-02-15.
+ */
+public class CommentEntryJson {
+    public UserProfile author;
+    public long timestamp = 0;
+    public String text = "";
+    public long id = 0;
+}
