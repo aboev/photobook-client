a6e4a47a3b33d91b6e9d3ba320104e0841d902b6
Aleksey Boev <aboyev@laptop>
2015-02-20 16:32:21 +0900

    - Added KEY_USERNAME and KEY_CONTACTKEY to preferences

--- app/src/main/java/com/freecoders/photobook/common/Preferences.java
+++ app/src/main/java/com/freecoders/photobook/common/Preferences.java
@@ -8,8 +8,12 @@ public final class Preferences {
 	private SharedPreferences settings;
 	
 	private String KEY_USERID = "userid";
+    private String KEY_USERNAME = "username";
+    private String KEY_CONTACTKEY = "contactkey";
 	
 	public String strUserID = "";
+    public String strUserName = "";
+    public String strContactKey = "";
 	
 	public Preferences(Context context) {
 		settings = context.getSharedPreferences(Constants.PREFS_NAME, 0);
@@ -17,12 +21,16 @@ public final class Preferences {
 	
 	public final Boolean loadPreferences(){
 		strUserID = settings.getString(KEY_USERID, "");
+        strUserName = settings.getString(KEY_USERNAME, "");
+        strContactKey = settings.getString(KEY_CONTACTKEY, "");
 		return strUserID.length() != 0;
 	}
 	
 	public final void savePreferences(){
 		SharedPreferences.Editor editor = settings.edit();
 		editor.putString(KEY_USERID, strUserID);
+        editor.putString(KEY_USERNAME, strUserName);
+        editor.putString(KEY_CONTACTKEY, strContactKey);
 		editor.commit();
 	}
 	
