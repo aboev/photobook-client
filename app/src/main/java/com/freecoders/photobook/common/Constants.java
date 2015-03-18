55e96bf6b267d1bebe336aef222ade998013ee66
Aleksey Boev <aboyev@laptop>
2015-03-18 20:53:58 +0900

    - Added SERVER_PATH_IMAGE_PRESIGNED_URL - Added PUSH_SENDER_ID

--- app/src/main/java/com/freecoders/photobook/common/Constants.java
+++ app/src/main/java/com/freecoders/photobook/common/Constants.java
@@ -9,6 +9,7 @@ public class Constants {
     public final static String SERVER_URL = "http://photobook-freecoders.rhcloud.com";
     public final static String SERVER_PATH_USER = "/user";
     public final static String SERVER_PATH_IMAGE = "/image";
+    public final static String SERVER_PATH_IMAGE_PRESIGNED_URL = "/image/upload_url";
     public final static String SERVER_PATH_CONTACTS = "/contacts";
     public final static String SERVER_PATH_FRIENDS = "/friends";
     public final static String SERVER_PATH_FEED = "/feed";
@@ -17,5 +18,6 @@ public class Constants {
 
     public final static String FILENAME_AVATAR = "avatar.jpg";
     public final static String APP_FOLDER = "photobook";
+    public final static String PUSH_SENDER_ID = "69387014044";
 }
 
