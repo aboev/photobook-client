5a2010f3320ca1d62c2a4986b8d378bea9f3947e
Aleksey Boev <aboyev@laptop>
2015-03-18 21:00:53 +0900

    GcmBroadcastReceiver initial commit

---
+++ app/src/main/java/com/freecoders/photobook/gcm/GcmBroadcastReceiver.java
@@ -0,0 +1,22 @@
+package com.freecoders.photobook.gcm;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+/**
+ * Created by Alex on 2015-03-08.
+ */
+public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Explicitly specify that GcmIntentService will handle the intent.
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                GcmIntentService.class.getName());
+        // Start the service, keeping the device awake while it is launching.
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+    }
+}
