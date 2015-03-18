be1aff7a1ab8a2ec0baba962923d3baaf7cbaa60
Aleksey Boev <aleksey.boev@samsung.com>
2015-03-18 19:47:36 +0900

    Added KEY_COMMENTS_TIMESTAMP and KEY_UNREAD_IMAGES

--- app/src/main/java/com/freecoders/photobook/common/Preferences.java
+++ app/src/main/java/com/freecoders/photobook/common/Preferences.java
@@ -4,6 +4,9 @@ package com.freecoders.photobook.common;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public final class Preferences {
 	private SharedPreferences settings;
 	
@@ -11,11 +14,15 @@ public final class Preferences {
     private String KEY_USERID_PUBLIC = "publicid";
     private String KEY_USERNAME = "username";
     private String KEY_CONTACTKEY = "contactkey";
+    private String KEY_COMMENTS_TIMESTAMP = "comments_timestamp";	//Last timestamp for comments request
+    private String KEY_UNREAD_IMAGES = "images_unread";
 
     public Integer intPublicID = 0;
 	public String strUserID = "";
     public String strUserName = "";
     public String strContactKey = "";
+    public String strCommentsTimestamp = "0";
+    public Set<String> hsetUnreadImages = new HashSet<String>();	//Image IDs that have unread comments
 	
 	public Preferences(Context context) {
 		settings = context.getSharedPreferences(Constants.PREFS_NAME, 0);
@@ -26,6 +33,8 @@ public final class Preferences {
 		strUserID = settings.getString(KEY_USERID, "");
         strUserName = settings.getString(KEY_USERNAME, "");
         strContactKey = settings.getString(KEY_CONTACTKEY, "");
+        strCommentsTimestamp = settings.getString(KEY_COMMENTS_TIMESTAMP, "0");
+        hsetUnreadImages = settings.getStringSet(KEY_UNREAD_IMAGES, new HashSet<String>());
 		return strUserID.length() != 0;
 	}
 	
@@ -35,7 +44,10 @@ public final class Preferences {
         editor.putString(KEY_USERNAME, strUserName);
         editor.putString(KEY_CONTACTKEY, strContactKey);
         editor.putInt(KEY_USERID_PUBLIC, intPublicID);
+        editor.putString(KEY_COMMENTS_TIMESTAMP, strCommentsTimestamp);
+        editor.putStringSet(KEY_UNREAD_IMAGES, hsetUnreadImages);
 		editor.commit();
 	}
 	
 }
+
