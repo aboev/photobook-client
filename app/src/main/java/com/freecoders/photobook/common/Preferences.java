73964f16c2fd42451c6c58b2da6e05dd8e1ff660
Aleksey Boev <boev.aleksey@gmail.com>
2015-03-20 19:12:18 +0900

    Added strPhone, strEmail, unreadImagesMap

--- app/src/main/java/com/freecoders/photobook/common/Preferences.java
+++ app/src/main/java/com/freecoders/photobook/common/Preferences.java
@@ -4,28 +4,44 @@ package com.freecoders.photobook.common;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.freecoders.photobook.gson.CommentEntryJson;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public final class Preferences {
 	private SharedPreferences settings;
+
+    Gson gson = new Gson();
 	
 	private String KEY_USERID = "userid";
     private String KEY_USERID_PUBLIC = "publicid";
     private String KEY_USERNAME = "username";
     private String KEY_CONTACTKEY = "contactkey";
+    private String KEY_PHONE = "phone";
+    private String KEY_EMAIL = "email";
     private String KEY_PUSH_REGID = "pushregid";
     private String KEY_COMMENTS_TIMESTAMP = "comments_timestamp"; //Last timestamp for comments request
     private String KEY_UNREAD_IMAGES = "images_unread";
+    private String KEY_UNREAD_IMAGES2 = "images_unread2";
     private String KEY_REGISTER_STATUS = "status";
 
     public Integer intPublicID = 0;
 	public String strUserID = "";
     public String strUserName = "";
     public String strContactKey = "";
+    public String strPhone = "";
+    public String strEmail = "";
     public String strPushRegID = "";
     public String strCommentsTimestamp = "0";
     public Set<String> hsetUnreadImages = new HashSet<String>(); //Image IDs that have unread comments
+    public HashMap<String, Integer> unreadImagesMap =
+            new HashMap<String, Integer>(); //Image IDs that have unread comments
     public Integer intRegisterStatus = 0;   // 0 - not registered, 
             // 1 - waiting for sms code, 2 registered
 	
@@ -38,9 +54,14 @@ public final class Preferences {
 		strUserID = settings.getString(KEY_USERID, "");
         strUserName = settings.getString(KEY_USERNAME, "");
         strContactKey = settings.getString(KEY_CONTACTKEY, "");
+        strPhone = settings.getString(KEY_PHONE, "");
+        strEmail = settings.getString(KEY_EMAIL, "");
         strPushRegID = settings.getString(KEY_PUSH_REGID, "");
         strCommentsTimestamp = settings.getString(KEY_COMMENTS_TIMESTAMP, "0");
         hsetUnreadImages = settings.getStringSet(KEY_UNREAD_IMAGES, new HashSet<String>());
+        Type type = new TypeToken<HashMap<String, Integer>>(){}.getType();
+        unreadImagesMap = gson.fromJson(
+                settings.getString(KEY_UNREAD_IMAGES2, "{}"), type);
         intRegisterStatus = settings.getInt(KEY_REGISTER_STATUS, 0);
 		return strUserID.length() != 0;
 	}
@@ -50,13 +71,15 @@ public final class Preferences {
 		editor.putString(KEY_USERID, strUserID);
         editor.putString(KEY_USERNAME, strUserName);
         editor.putString(KEY_CONTACTKEY, strContactKey);
+        editor.putString(KEY_PHONE, strPhone);
+        editor.putString(KEY_EMAIL, strEmail);
         editor.putInt(KEY_USERID_PUBLIC, intPublicID);
         editor.putString(KEY_PUSH_REGID, strPushRegID);
         editor.putString(KEY_COMMENTS_TIMESTAMP, strCommentsTimestamp);
         editor.putStringSet(KEY_UNREAD_IMAGES, hsetUnreadImages);
+        editor.putString(KEY_UNREAD_IMAGES2, gson.toJson(unreadImagesMap));
         editor.putInt(KEY_REGISTER_STATUS, intRegisterStatus);
 		editor.commit();
 	}
 	
 }
-
