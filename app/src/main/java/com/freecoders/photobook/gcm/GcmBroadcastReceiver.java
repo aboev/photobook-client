84db88c688e8d7324cae6b48ee462c2ec0aa7bc0
Aleksey Boev <aboyev@laptop>
2015-03-20 20:40:03 +0900

    - Updated showNotification() to handle json properly

--- app/src/main/java/com/freecoders/photobook/gcm/GcmBroadcastReceiver.java
+++ app/src/main/java/com/freecoders/photobook/gcm/GcmBroadcastReceiver.java
@@ -6,7 +6,9 @@ import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.WakefulBroadcastReceiver;
 import android.util.Log;
@@ -37,16 +39,11 @@ public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
                     MESSAGE_TYPE_DELETED.equals(messageType)) {
             } else if (GoogleCloudMessaging.
                     MESSAGE_TYPE_MESSAGE.equals(messageType)) {
-                try {
-                    JSONObject resJson = new JSONObject(extras.toString());
-                    Log.d(Constants.LOG_TAG, "Received push message " + extras.toString());
-                    if (resJson.has("event") && resJson.has("msg")) {
-                       Integer intEventType = resJson.getInt("event");
-                       String strData = resJson.getString("msg");
-                       showNotification(context, intEventType, strData);
-                    }
-                } catch (JSONException e) {
-                    Log.d(Constants.LOG_TAG, "JSON parsing error");
+                Log.d(Constants.LOG_TAG, "Received push message " + extras.toString());
+                if (extras.containsKey("event") && extras.containsKey("msg")) {
+                   Integer intEventType = extras.getInt("event");
+                   String strData = extras.getString("msg");
+                   showNotification(context, intEventType, strData);
                 }
             }
         }
@@ -56,8 +53,11 @@ public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
     public void showNotification(Context context, Integer intEventType, String strData) {
         String strMessage = "";
         try {
+            Log.d(Constants.LOG_TAG, "Parsing " + strData);
             JSONObject resJson = new JSONObject(strData);
-            JSONObject author = resJson.getJSONObject("author");
+            String strAuthor = resJson.getString("author").replaceAll("\\\"", "\"");
+            JSONObject author = new JSONObject(strAuthor);
+            Log.d(Constants.LOG_TAG, "Author =  " + strAuthor);
             String strText = resJson.getString("text");
             strMessage = strText;
             if (author.has("name"))
@@ -72,18 +72,23 @@ public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
         Intent i = new Intent(context, MainActivity.class);
         i.putExtra("event_type", intEventType);
         i.putExtra("data", strData);
+        i.addFlags( Intent.FLAG_ACTIVITY_SINGLE_TOP );
+        i.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK );
 
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
-                new Intent(context, MainActivity.class), 0);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, i,
+                PendingIntent.FLAG_UPDATE_CURRENT);
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(context)
                         .setSmallIcon(R.drawable.ic_launcher)
-                        .setContentTitle("Photobook")
+                        .setContentTitle(context.getResources().
+                                getString(R.string.notification_new_comment))
                         .setStyle(new NotificationCompat.BigTextStyle()
                                 .bigText(strMessage))
                         .setContentText(strMessage)
-                        .setAutoCancel(true);
+                        .setAutoCancel(true)
+                        .setVibrate(new long[] { 200, 500, 200 })
+                        .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
 
         mBuilder.setContentIntent(contentIntent);
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
