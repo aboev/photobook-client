b91e517df913bdf1c1e45caa56229bf254a46b9e
Aleksey Boev <boev.aleksey@gmail.com>
2015-03-24 19:21:05 +0900

    Implemented setNullFields, CommentEntryJson(int method); Added reply_to

--- app/src/main/java/com/freecoders/photobook/gson/CommentEntryJson.java
+++ app/src/main/java/com/freecoders/photobook/gson/CommentEntryJson.java
@@ -1,5 +1,9 @@
 package com.freecoders.photobook.gson;
 
+import com.android.volley.Request;
+
+import org.apache.http.HttpRequest;
+
 /**
  * Created by Alex on 2015-02-15.
  */
@@ -10,4 +14,21 @@ public class CommentEntryJson {
     public String author_id = "";
     public String image_id = "";
     public long id = 0;
+    public long reply_to = 0;
+
+    public CommentEntryJson() {
+    }
+
+    public CommentEntryJson(int method) {
+        if (method == Request.Method.POST) {
+            author = null;
+            author_id = null;
+        }
+    }
+
+    public void setNullFields(){
+        if (text.isEmpty()) text = null;
+        if (author_id.isEmpty()) author_id = null;
+        if (image_id.isEmpty()) image_id = null;
+    }
 }
