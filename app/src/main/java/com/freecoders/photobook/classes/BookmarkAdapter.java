d1648f524c7a028540c29ef3dff5f31cb7ace889
Aleksey Boev <boev.aleksey@gmail.com>
2015-03-25 21:42:24 +0900

    - Implemented getView method

--- app/src/main/java/com/freecoders/photobook/classes/BookmarkAdapter.java
+++ app/src/main/java/com/freecoders/photobook/classes/BookmarkAdapter.java
@@ -1,8 +1,12 @@
 package com.freecoders.photobook.classes;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.freecoders.photobook.R;
 
 import java.util.ArrayList;
 
@@ -11,12 +15,12 @@ import java.util.ArrayList;
  */
 public class BookmarkAdapter {
     private Context context;
-    private ArrayList<String> items;
+    private String[] items;
     private ViewGroup parentView;
     private onItemSelectedListener listener;
     public int selectedPosition = 0;
 
-    public BookmarkAdapter(Context context, ViewGroup parentView, ArrayList<String> items) {
+    public BookmarkAdapter(Context context, ViewGroup parentView, String[] items) {
         this.context = context;
         this.items = items;
         this.parentView = parentView;
@@ -24,21 +28,32 @@ public class BookmarkAdapter {
     }
 
     private View getView(final int position) {
-        View v = new View(context);
-        v.setOnClickListener(new View.OnClickListener() {
+        String inflater = Context.LAYOUT_INFLATER_SERVICE;
+        LayoutInflater vi = (LayoutInflater)context.getSystemService(inflater);
+        View view;
+        if (position == selectedPosition)
+            view = vi.inflate(R.layout.item_bookmark_selected, null);
+        else
+            view = vi.inflate(R.layout.item_bookmark, null);
+        TextView tv = (TextView)view.findViewById(R.id.txtViewBookmark);
+        tv.setText(items[position]);
+
+        view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                selectedPosition = position;
+                redraw();
                 if (listener != null)
                     listener.onItemSelected(position);
             }
         });
-        return v;
+        return view;
     }
 
     private void redraw(){
         if (parentView.getChildCount() > 0)
             parentView.removeAllViews();
-        for (int i = 0; i < items.size(); i++) {
+        for (int i = 0; i < items.length; i++) {
             View view = getView(i);
             parentView.addView(view);
         }
