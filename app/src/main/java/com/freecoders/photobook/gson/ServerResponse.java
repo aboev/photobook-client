6178a7644b0faf6473c9bdb6c6fb35b951ab5533
Aleksey Boev <boev.aleksey@gmail.com>
2015-03-30 20:48:49 +0900

    Added timestamp

--- app/src/main/java/com/freecoders/photobook/gson/ServerResponse.java
+++ app/src/main/java/com/freecoders/photobook/gson/ServerResponse.java
@@ -9,6 +9,7 @@ public class ServerResponse<T> {
     public String result = "";
     public T data = null;
     public Integer code = 0;
+    public String timestamp = "0";
 
     public Boolean isSuccess () {
         return ((result != null) && result.equals(Constants.RESPONSE_RESULT_OK));
